c650fd5c45e6039da56be206f50c0a0099911f9f
chris <chris@chris-pc>
2020-02-18 18:18:30 -0600

    Added that get the artwork availability status and the name of the artwork

--- get.py
+++ get.py
@@ -1,4 +1,4 @@
-"""Groups all the prompts for neater apearance
+"""Groups all the repeating prompts and prints for neater apearance
 """
 
 # Prompts the user to enter the name and email of the artist
@@ -16,12 +16,35 @@ def artwork_info():
 
     return name, artwork, price
 
+# Prompts the user to enter artist name
 def artist_name():
     name = input('\nEnter artist name: ')
 
     return name
 
+# Prompts the user to enter artist email
 def artist_email():
     email = input('\nEnter artist email: ')
 
     return email
+
+# Prompts the user to enter artwork name
+def artwork_name():
+    artwork = input('\nEnter the artworks\'s name: ')
+
+    return artwork
+
+# Prints information for artworks that are available 
+def for_sale_artwork(r):
+    print(f"\nArtist: {r['artist']}\nArtwork: {r['name']}\nPrice: ${r['price']}\nStatus: for sale")
+
+# Prints information for artworks that are sold
+def sold_artwork(r):
+    print(f"\nArtist: {r['artist']}\nArtwork: {r['name']}\nPrice: ${r['price']}\nStatus: sold")
+
+# Prompts the user to enter the status of an artwork
+def artwork_availability():
+    print('\nOptions: sold or for sale')
+    status = input('Enter the availability of the artwork: ')
+
+    return status
